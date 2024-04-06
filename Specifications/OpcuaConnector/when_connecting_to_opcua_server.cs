9f2e46dcd0151826c7aa6e77a1efd8d408f53c42
Rafael Schlatter <rafaelschlatter@gmail.com>
2024-04-06 08:52:59 +0200

    Add blank test to make action pass

---
+++ Specifications/OpcuaConnector/when_connecting_to_opcua_server.cs
@@ -0,0 +1,13 @@
+using Machine.Specifications;
+
+namespace RaaLabs.Edge.Connectors.OPCUA.Specs.OPCUAConnector;
+
+[Tags("OPCUAConnector")]
+[Subject("OPCUAConnector")]
+public class when_receiving_mqtt_event
+{
+    Establish context = () => { };
+    Because of = () => { };
+
+    It should_do_stuff = () => { };
+}
