24b335a8251b51e2209429a8513e0803cc9da074
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2024-06-14 15:19:11 +0200

    Time with zone?

--- Specifications/for_CreateDatapointsFromDataValues/when_creating_datapoints/and_statuscode_not_good.cs
+++ Specifications/for_CreateDatapointsFromDataValues/when_creating_datapoints/and_statuscode_not_good.cs
@@ -13,9 +13,9 @@ public class and_statuscode_not_good : given.a_creator
     {
         clock
             .Setup(_ => _.GetUtcNow())
-            .Returns(new DateTimeOffset(new DateTime(2024, 1, 1)));
+            .Returns(new DateTimeOffset(new DateTime(2024, 1, 1, 1, 1, 1, DateTimeKind.Utc)));
         
-        nodevalue = new NodeValue(new NodeId("ns=1;i=1"), new (){Value = 2, StatusCode = StatusCodes.BadNodeIdInvalid, SourceTimestamp = new DateTime(2024, 1, 1), ServerTimestamp = new DateTime(2024, 1, 1)});
+        nodevalue = new NodeValue(new NodeId("ns=1;i=1"), new (){Value = 2, StatusCode = StatusCodes.BadNodeIdInvalid, SourceTimestamp = new DateTime(2024, 1, 1, 1, 1, 1, DateTimeKind.Utc), ServerTimestamp = new DateTime(2024, 1, 1, 1, 1, 1, DateTimeKind.Utc)});
     };
 
     static OpcuaDatapointOutput result;
@@ -25,5 +25,5 @@ public class and_statuscode_not_good : given.a_creator
     It should_have_correct_source = () => result.Source.ShouldEqual("OPCUA");
     It should_have_correct_tag = () => result.Tag.ShouldEqual("ns=1;i=1");
     It should_have_correct_value = () => result.Value.Equals(2);
-    It should_have_correct_timestamp = () => result.Timestamp.ShouldEqual(1704063600000);
+    It should_have_correct_timestamp = () => result.Timestamp.ShouldEqual(1704070861000);
 }
