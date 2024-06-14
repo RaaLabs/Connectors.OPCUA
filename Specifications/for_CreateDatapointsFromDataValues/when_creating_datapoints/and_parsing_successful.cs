1ece0b920cd7bd1f3798558ec47fa2d9301c7d79
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2024-06-14 15:23:41 +0200

    Fix the other tests

--- Specifications/for_CreateDatapointsFromDataValues/when_creating_datapoints/and_parsing_successful.cs
+++ Specifications/for_CreateDatapointsFromDataValues/when_creating_datapoints/and_parsing_successful.cs
@@ -15,9 +15,9 @@ public class and_parsing_successful : given.a_creator
     {
         clock
             .Setup(_ => _.GetUtcNow())
-            .Returns(new DateTimeOffset(new DateTime(2024, 1, 1)));
+            .Returns(new DateTimeOffset(new DateTime(2024, 1, 1, 1, 1, 1, DateTimeKind.Utc)));
         
-        nodevalue = new NodeValue("ns=1;i=1", new (){Value = 2, StatusCode = StatusCodes.Good, SourceTimestamp = new DateTime(2024, 1, 1), ServerTimestamp = new DateTime(2024, 1, 1)});
+        nodevalue = new NodeValue("ns=1;i=1", new (){Value = 2, StatusCode = StatusCodes.Good, SourceTimestamp = new DateTime(2024, 1, 1, 1, 1, 1, DateTimeKind.Utc), ServerTimestamp = new DateTime(2024, 1, 1, 1, 1, 1, DateTimeKind.Utc)});
     };
 
     static OpcuaDatapointOutput result;
@@ -27,5 +27,5 @@ public class and_parsing_successful : given.a_creator
     It should_have_correct_source = () => result.Source.ShouldEqual("OPCUA");
     It should_have_correct_tag = () => result.Tag.ShouldEqual("ns=1;i=1");
     It should_have_correct_value = () => result.Value.Equals(2);
-    It should_have_correct_timestamp = () => result.Timestamp.ShouldEqual(1704063600000);
+    It should_have_correct_timestamp = () => result.Timestamp.ShouldEqual(1704070861000);
 }
